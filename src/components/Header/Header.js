ff1c3813265f9595e08099f6ea82bdd1b02c4f92
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-04-04 18:48:07 +0600

    navbar responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,33 @@
-import React from "react";
-// import { CustomLink } from "react-router-dom";
+import React, { useState } from "react";
 import CustomLink from "../CustomLink/CustomLink";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
 import "./Header.css";
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="navbar bg-teal-400 p-8 mb-8">
-      <CustomLink to="/"> Home</CustomLink>
-      <CustomLink to="/reviews">Reviews</CustomLink>
-      <CustomLink to="/dashboard">Dashboard</CustomLink>
-      <CustomLink to="/blogs">Blogs</CustomLink>
-      <CustomLink to="/about">About</CustomLink>
-    </div>
+    <nav>
+      <div
+        onClick={() => setOpen(!open)}
+        className=" md:hidden bg-teal-400 p-6 mb-3"
+      >
+        {open ? (
+          <XIcon className="w-8 h-8" />
+        ) : (
+          <MenuIcon className="w-8 h-8" />
+        )}
+      </div>
+      <div
+        className={`navbar bg-teal-400 p-8 mb-8 md:flex justify-center absolute md:static duration-300 ease-in ${
+          open ? " top-16 rounded-md flex flex-col " : "top-[-250px]"
+        }`}
+      >
+        <CustomLink to="/"> Home</CustomLink>
+        <CustomLink to="/reviews">Reviews</CustomLink>
+        <CustomLink to="/dashboard">Dashboard</CustomLink>
+        <CustomLink to="/blogs">Blogs</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </div>
+    </nav>
   );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,20 @@ import NikonD780 from "../Assets/NikonD780.jpg";
 import "./Home.css";
 const Home = () => {
   return (
-    <div className="grid grid-cols-2 max-w-7xl mx-auto justify-evenly items-center">
-      <div className="ml-8">
+    <div className="grid lg:grid-cols-2 sm:grid-cols-1 max-w-7xl mx-auto justify-evenly items-center">
+      <div className="ml-8 flex flex-col items-center lg:items-start sm:items-center md:items-center">
         <h1 className=" col text-5xl font-bold mb-6 text-left text-teal-600">
           Select Your Best Camera
         </h1>
-        <p className="text-xl font-semibold text-left w-11/12">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis facilis
-          magnam voluptas! Vel ex illum nesciunt quo repellat similique qui
-          perferendis recusandae blanditiis nisi quam id eveniet fugiat, sed
-          iusto.
+        <p className="text-xl font-semibold text-left w-11/12 mb-6">
+          The D780 boasts a 24.5MP backside illuminated FX full frame sensor,
+          capable of remarkable light gathering for highly detailed photos and
+          videos with ultra shallow depth of field and sharp, clean low light
+          shots. Welcome to the brilliance of full frame.
         </p>
+        <button className=" bg-teal-600 py-2 mb-10 sm:mb-10 rounded-md text-white text-xl font-semibold w-36">
+          Live Demo
+        </button>
       </div>
       <div>
         <img className=" w-3/4 mx-auto" src={NikonD780} alt="" />
